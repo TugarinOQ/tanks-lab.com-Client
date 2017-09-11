b4709466f18b1e841a890743ec1b8d73c976ff43
Dmitry Streltsov <dev-0x15@yandex.by>
2017-09-11 11:37:23 +0300

    Fix url to Api Server

--- src/app/configs/urls.config.ts
+++ src/app/configs/urls.config.ts
@@ -1,6 +1,6 @@
 export const urls__config = {
-  // hostLocal: 'https://api.tanks-lab.com',
-  hostLocal: 'http://localhost:8080',
+  hostLocal: 'https://api.tanks-lab.com',
+  // hostLocal: 'http://localhost:8080',
   users: {
     auth: '/users/login',
     reg: '/users/register',
